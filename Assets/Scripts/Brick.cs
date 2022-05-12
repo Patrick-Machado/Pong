ca2841ebe402a57a8c5d63680f18437d8068539a
Patrick-Machado <formalemailptk@gmail.com>
2022-05-12 03:21:43 -0300

    Different Bricks On Scenes Common Concrete and Metal Brick

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -12,7 +12,7 @@ public class Brick : MonoBehaviour
         level_Bricks = GameObject.FindGameObjectWithTag("Level_Ctrl").GetComponent<Level_Bricks>();
     }
 
-    bool notcallReduceTwice = false;
+    bool notcallReduceTwice = false; int hitcount = 0; bool onDelay;
     public void Ball_Hit()
     {
         if(mBrick_Type == Brick.Brick_Type.common)
@@ -22,11 +22,36 @@ public class Brick : MonoBehaviour
             notcallReduceTwice = true;
             Destroy(gameObject);
         }
+        else if (mBrick_Type == Brick.Brick_Type.concrete) // neets two hits to be destroyed
+        {
+            if (!onDelay)
+            {
+                hitcount += 1;
+                onDelay = true;
+                Invoke("removeOnDelay", .8f);
+            }
+            if(hitcount > 1) // when the brick is hit twice
+            {
+                if (!notcallReduceTwice) { level_Bricks.Reduce_A_Brick(); }
+
+                notcallReduceTwice = true;
+                Destroy(gameObject);
+            }
+            
+        }
+        else if(mBrick_Type == Brick.Brick_Type.metal)// is indestructible so only bounces
+        {
+
+        }
     }
 
+    void removeOnDelay()
+    {
+        onDelay = false;
+    }
 
     public enum Brick_Type
     {
-        common, metal, 
+        common, metal, concrete
     }
 }
